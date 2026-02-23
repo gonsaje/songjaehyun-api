5526f7055f8dd808a68cf8e7b8948832e8fe42ed
gonsaje <song.jaehyun@gmail.com>
2026-02-23 15:13:44 -0500

    first commit

---
+++ src/test/java/com/songjaehyun/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songjaehyun.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
