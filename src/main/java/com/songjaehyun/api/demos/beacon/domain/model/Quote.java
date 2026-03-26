d5e0dfd6c94e842f4a55653556bba2759ee77b88
gonsaje <song.jaehyun@gmail.com>
2026-03-26 11:20:00 -0400

    add Quote model constructor and getters

--- src/main/java/com/songjaehyun/api/demos/beacon/domain/model/Quote.java
+++ src/main/java/com/songjaehyun/api/demos/beacon/domain/model/Quote.java
@@ -1,5 +1,31 @@
 package com.songjaehyun.api.demos.beacon.domain.model;
 
 public class Quote {
+    private final Symbol symbol;
+    private final double price;
+    private final long asOf; 
+    private final String source;
 
+    Quote(Symbol symbol, double price, long asOf, String source) {
+        this.symbol = symbol;
+        this.price = price;
+        this.asOf = asOf;
+        this.source = source;
+    }
+
+    public Symbol getSymbol() {
+        return symbol;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public long getAsOf() {
+        return asOf;
+    }
+
+    public String getSource() {
+        return source;
+    }
 }
